3f1eee37415bc20f6fb807e3edc0e26be64a2dc9
Richard Oven <71844377+rvnlive@users.noreply.github.com>
2021-10-07 23:31:12 +0100

    -Compression added to Webpack

--- vue.config.js
+++ vue.config.js
@@ -1,24 +1,3 @@
 module.exports = {
   publicPath: './',
-  pluginOptions: {
-    compression: {
-      brotli: {
-        filename: '[file].br[query]',
-        algorithm: 'brotliCompress',
-        include: /\.(js|css|html|svg|json)(\?.*)?$/i,
-        compressionOptions: {
-          params: {
-            [zlib.constants.BROTLI_PARAM_QUALITY]: 11,
-          },
-        },
-        minRatio: 0.8,
-      },
-      gzip: {
-        filename: '[file].gz[query]',
-        algorithm: 'gzip',
-        include: /\.(js|css|html|svg|json)(\?.*)?$/i,
-        minRatio: 0.8,
-      }
-    }
-  }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -23,5 +23,14 @@ module.exports = {
         }
       }
     ]
-  }
+  },
+  plugins: [
+    new CompressionPlugin({
+      filename: "[path][base].gz",
+      algorithm: "gzip",
+      test: /\.js$|\.css$|\.html$/,
+      threshold: 10240,
+      minRatio: 0.8,
+    }),
+  ],
 }
