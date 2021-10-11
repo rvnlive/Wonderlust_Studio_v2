599c3468142af7582b3c0f80eed7b9ce0fc3775f
Richard Oven <71844377+rvnlive@users.noreply.github.com>
2021-10-11 11:42:08 +0100

    -Added PostCSS (config setup)

--- postcss.config.js
+++ postcss.config.js
@@ -3,25 +3,25 @@ const purgecss = require("@fullhuman/postcss-purgecss")
 module.exports = {
   plugins: [
     require('autoprefixer'),
-  //   purgecss({
-  //     content: [`./public/**/*.html`, `./src/**/*.vue`],
-  //     defaultExtractor(content) {
-  //       const contentWithoutStyleBlocks = content.replace(
-  //         /<style[^]+?<\/style>/gi,
-  //         ""
-  //       );
-  //       return (
-  //         contentWithoutStyleBlocks.match(
-  //           /[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g
-  //         ) || []
-  //       );
-  //     },
-  //     safelist: [
-  //       /-(leave|enter|appear)(|-(to|from|active))$/,
-  //       /^(?!(|.*?:)cursor-move).+-move$/,
-  //       /^router-link(|-exact)-active$/,
-  //       /data-v-.*/,
-  //     ],
-  //   }),
+    purgecss({
+      content: [`./public/**/*.html`, `./src/**/*.vue`],
+      defaultExtractor(content) {
+        const contentWithoutStyleBlocks = content.replace(
+          /<style[^]+?<\/style>/gi,
+          ""
+        );
+        return (
+          contentWithoutStyleBlocks.match(
+            /[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g
+          ) || []
+        );
+      },
+      safelist: [
+        /-(leave|enter|appear)(|-(to|from|active))$/,
+        /^(?!(|.*?:)cursor-move).+-move$/,
+        /^router-link(|-exact)-active$/,
+        /data-v-.*/,
+      ],
+    }),
   ]
 };
