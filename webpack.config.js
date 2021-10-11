3b98ca749154d7b9486673484c58a200cd000780
Richard Oven <71844377+rvnlive@users.noreply.github.com>
2021-10-11 11:03:17 +0100

    -Added PostCSS loader (webpack settings)

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,24 @@ module.exports = {
             }
           }
         ]
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+            }
+          },
+          {
+            loader: 'postcss-loader'
+          }
+        ]
       }
     ],
     loaders: [
