05ad645804f0fc510effb8e9c39d708c49ed0884
Richard Oven <71844377+rvnlive@users.noreply.github.com>
2021-10-22 01:19:28 +0100

    Maintenance: - PurgeCSS config change

--- purgecss.config.js
+++ purgecss.config.js
@@ -2,8 +2,8 @@ module.exports = {
     content: ['./dist/index.html', './dist/**/*.html', './dist/js/*.js'],
     css: ['./dist/css/*.css'],
     output: ['./dist/css/'],
-    keyframes: true,
-    variables: true,
+    // keyframes: true,
+    // variables: true,
     safelist: {
         standard: [/fixed-.*/],
     }
